c2fe4be7b942c7b9e541071bc9cad0c70a95eb10
stans184 <59383072+stans184@users.noreply.github.com>
2020-05-24 05:45:59 +0900

    Update extractMC.py

--- extractMC.py
+++ extractMC.py
@@ -13,6 +13,9 @@ def extractARMSMC(location):
         # find my value 'use' in listed line, returne the value, save
         # i can find the line because (i already know how does it look like)
         # can i found without known of the file?
+        
+        # if data_list.index('Robot') == 0 and data_list.index('In') ==1
+        # if 'Robot' and 'In' and 'Use' in data_list
         try:
             while i<2:
                 if data_list.index('Use') == 2:
