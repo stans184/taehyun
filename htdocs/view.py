e31c61fd650a4b3cb63838ece35147a9ddb701d4
stans184 <59383072+stans184@users.noreply.github.com>
2020-05-31 05:09:30 +0900

    html_sanitizer

--- htdocs/view.py
+++ htdocs/view.py
@@ -1,8 +1,10 @@
-import os
+import os, html_sanitizer
 
 def getList():
+  sanitizer = html_sanitizer.Sanitizer()
   files = os.listdir('data')          # data 폴더 안의 파일 이름들을 가져와서 files에 list로 저장
   liststr = ''
   for item in files:
+    item = sanitizer.sanitize(itme)
     liststr = liststr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
   return liststr
