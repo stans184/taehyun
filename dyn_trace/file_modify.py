bac0d94179d5b2c22c9539250f472266af936f07
stans184 <life4603@naver.com>
2020-10-05 00:27:41 +0900

    201004

--- dyn_trace/file_modify.py
+++ dyn_trace/file_modify.py
@@ -3,28 +3,29 @@ import shutil
 import csv
 import pandas as pd
 
-# 파일 확장자 변경 gh -> csv
-# shutil.move('d:/workspace/trace/autosave_tracedata_00 - 복사본.gh', 'd:/workspace/trace/autosave_tracedata_00 - 복사본.csv')
-with open('d:/workspace/trace/autosave_tracedata_00 - 복사본.gh') as f:
-    # raw_data 가공
-    raw_data = f.read().splitlines()
-    raw_data = list(filter(None, raw_data))
+def chg_csv(machine):
+    # 넘어온 trace file open
+    with open('c:/dyn_trace_temp/' + machine + '_autosave_tracedata_00.gh') as f:
 
-    # trace 항목 구역 설정
-    start = raw_data.index('[Sec]') + 1
-    end = int(raw_data.index('2048'))
-    trace_item = raw_data[start:end]
-    trace_item.insert(0, 'time')
+        # raw data 가공
+        raw_data = f.read().splitlines()
+        raw_data = list(filter(None, raw_data))
 
-    # trace_data 가공 / 공백 제거
-    trace_raw_data = raw_data[end+2:]
-    trace_data = []
-    i=0
-    while i<len(trace_raw_data):
-        trace_data.insert(i,trace_raw_data[i].split())
-        i = i+1
+        # trace 항목 구역 설정
+        start = raw_data.index('[Sec]') + 1
+        end = int(raw_data.index('2048'))
+        trace_item = raw_data[start:end]
+        trace_item.insert(0, 'time')
 
-    # csv 파일로 저장
-    trace_data.insert(0,trace_item)
-    dataframe = pd.DataFrame(trace_data)
-    dataframe.to_csv('c:/dyn_trace_temp/trace.csv', header=False, index=False)
+        # trace_data 가공 / 공백 제거
+        trace_raw_data = raw_data[end+2:]
+        trace_data = []
+        i=0
+        while i<len(trace_raw_data):
+            trace_data.insert(i,trace_raw_data[i].split())
+            i = i+1
+
+        # csv 파일로 저장
+        trace_data.insert(0,trace_item)
+        dataframe = pd.DataFrame(trace_data)
+        dataframe.to_csv('c:/dyn_trace_temp/' + machine + '_trace.csv', header=False, index=False)
--- dyn_trace/visualize.py
+++ dyn_trace/visualize.py
@@ -5,63 +5,65 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-# data를 불러와서, pandas로 사용하기 위한 dataframe으로 변경
-rawdata = pd.read_csv('c:/dyn_trace_temp/trace.csv')
-df = pd.DataFrame(rawdata)
+def graph(machine):
+    # data를 불러와서, pandas로 사용하기 위한 dataframe으로 변경
+    rawdata = pd.read_csv('c:/dyn_trace_temp/' + machine + '_trace.csv')
+    df = pd.DataFrame(rawdata)
 
-# graph declare
-fig = plt.figure(figsize=(15,10))
-i=1
+    # graph declare
+    fig = plt.figure(figsize=(15,10))
+    i=1
 
-# RS act motor signal
-if "RS ZS-Z1 Act Motor Out" in df:
-    ax1=fig.add_subplot(5,1,i)
-    ax0=ax1.twinx()
-    ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
-    ax1.plot(df["RS ZS-Z1 Act Motor Out"], label='RS ZS-Z1 Act Motor Out')
-    ax1.plot(df["RS ZS-Z2 Act Motor Out"], label='RS ZS-Z2 Act Motor Out')
-    ax1.plot(df["RS ZS-Z3 Act Motor Out"], label='RS ZS-Z3 Act Motor Out')
-    ax1.legend(loc="lower left")
-    i = i + 1
+    # RS act motor signal
+    if "RS ZS-Z1 Act Motor Out" in df:
+        ax1=fig.add_subplot(5,1,i)
+        ax0=ax1.twinx()
+        ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
+        ax1.plot(df["RS ZS-Z1 Act Motor Out"], label='RS ZS-Z1 Act Motor Out')
+        ax1.plot(df["RS ZS-Z2 Act Motor Out"], label='RS ZS-Z2 Act Motor Out')
+        ax1.plot(df["RS ZS-Z3 Act Motor Out"], label='RS ZS-Z3 Act Motor Out')
+        ax1.legend(loc="lower left")
+        i = i + 1
 
-# RS lens acc
-if "RS Lens acc Y Converted Value" in df:
-    ax2=fig.add_subplot(5,1,i)
-    ax0=ax2.twinx()
-    ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
-    ax2.plot(df["RS Lens acc Y Converted Value"], label='RS Lens acc Y Converted Value')
-    ax2.plot(df["RS Lens acc X Converted Value"], label='RS Lens acc X Converted Value')
-    ax2.legend(loc="lower left")
-    i = i + 1
+    # RS lens acc
+    if "RS Lens acc Y Converted Value" in df:
+        ax2=fig.add_subplot(5,1,i)
+        ax0=ax2.twinx()
+        ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
+        ax2.plot(df["RS Lens acc Y Converted Value"], label='RS Lens acc Y Converted Value')
+        ax2.plot(df["RS Lens acc X Converted Value"], label='RS Lens acc X Converted Value')
+        ax2.legend(loc="lower left")
+        i = i + 1
 
-# RS Cap Z2 Converted Value
-if "RS Cap Z2 Converted Value" in df:
-    ax4=fig.add_subplot(5,1,i)
-    ax0=ax4.twinx()
-    ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
-    ax4.plot(df["RS Cap Z2 Converted Value"], label='RS Cap Z2 Converted Value')
-    ax4.plot(df["RS Cap Z1 Converted Value"], label='RS Cap Z1 Converted Value')
-    ax4.legend(loc="lower left")
-    i = i + 1
+    # RS Cap Z2 Converted Value
+    if "RS Cap Z2 Converted Value" in df:
+        ax4=fig.add_subplot(5,1,i)
+        ax0=ax4.twinx()
+        ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
+        ax4.plot(df["RS Cap Z2 Converted Value"], label='RS Cap Z2 Converted Value')
+        ax4.plot(df["RS Cap Z1 Converted Value"], label='RS Cap Z1 Converted Value')
+        ax4.legend(loc="lower left")
+        i = i + 1
 
-# stage position err X
-if "RS SS-X Position Error" in df:
-    ax3=fig.add_subplot(5,1,i)
-    ax0=ax3.twinx()
-    ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
-    ax3.plot(df["RS SS-X Position Error"], label='RS SS-X Position Error')
-    ax3.plot(df["WS SS-X Position Error"]*4, label='WS SS-X Position Error*4')
-    ax3.legend(loc="lower left")
-    i = i + 1
-# stage position err Y
-if "RS SS-Y Position Error" in df:
-    ax3=fig.add_subplot(5,1,i)
-    ax0=ax3.twinx()
-    ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
-    ax3.plot(df["RS SS-Y Position Error"], label='RS SS-Y Position Error')
-    ax3.plot(df["WS SS-Y Position Error"]*4, label='WS SS-Y Position Error*4')
-    ax3.legend(loc="lower left")
-    i = i + 1
+    # stage position err X
+    if "RS SS-X Position Error" in df:
+        ax3=fig.add_subplot(5,1,i)
+        ax0=ax3.twinx()
+        ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
+        ax3.plot(df["RS SS-X Position Error"], label='RS SS-X Position Error')
+        ax3.plot(df["WS SS-X Position Error"]*4, label='WS SS-X Position Error*4')
+        ax3.legend(loc="lower left")
+        i = i + 1
 
-# graph를 보여주기 위해
-plt.show()
+    # stage position err Y
+    if "RS SS-Y Position Error" in df:
+        ax3=fig.add_subplot(5,1,i)
+        ax0=ax3.twinx()
+        ax0.plot(df["WS LS SBC Sync. State"], label='WS LS SBC Sync. State')
+        ax3.plot(df["RS SS-Y Position Error"], label='RS SS-Y Position Error')
+        ax3.plot(df["WS SS-Y Position Error"]*4, label='WS SS-Y Position Error*4')
+        ax3.legend(loc="lower left")
+        i = i + 1
+
+    # graph를 보여주기 위해
+    plt.show()
